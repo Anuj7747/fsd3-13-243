f6e9310a3b0a1799588a98460737944b61c5ea48
Anuj <anujku9919774769@gmail.com>
2026-07-22 10:44:01 +0530

    class work

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,1 +1,2 @@
 console.log("Hello Node JS ");
+console.log("node js as backend  ");
---
+++ node.js
@@ -0,0 +1,6 @@
+function sayHello(name) {
+    console.log(`welcome to ${name}`);
+}
+const sayhi=
+sayHello('Anuj')
+console.log('sum of 2 and 5 is ${sum(2+5)}');
