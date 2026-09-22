22f29feb3c20b3d25ed6a797aac78be94e463669
Anuj <anujku9919774769@gmail.com>
2026-09-22 14:28:15 +0530

    add product

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,5 +1,5 @@
 import http from "http";
-import { getAllProducts } from "./products.js";
+import { getAllProducts , addProduct} from "./products.js";
 import { count } from "console";
 
 const server = http.createServer((req, res) => {
@@ -21,9 +21,10 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const product = JSON.parse(body);
-      console.log("received product:", product);
+      const item=addProduct(product);
       res.statusCode = 201;
-      res.end(JSON.stringify({ msg: "product added", product }));
+      res.end(JSON.stringify({ msg: "product added", data: 
+        item }));
     });
   } 
   else if (req.url.startsWith( "/products/") && req.method === "PUT") {
